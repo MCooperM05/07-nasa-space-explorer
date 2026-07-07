77e514f605c5e499b832562f9dafaa2d93fd1859
Michael Cooper Murphy <143638247+MCooperM05@users.noreply.github.com>
2026-07-07 13:42:41 -0400

    Add URL construction for NASA API with date range

--- js/script.js
+++ js/script.js
@@ -7,3 +7,5 @@ const endInput = document.getElementById('endDate');
 // - Default to a range of 9 days (from 9 days ago to today)
 // - Restrict dates to NASA's image archive (starting from 1995)
 setupDateInputs(startInput, endInput);
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key=Hnb4rGRJ7FnMaWWcS5GSjGxjjzAkTs9GzeZr5I8c&start_date=${startInput}&end_date=${endInput}';
